90b40db2f3b6bc9ede03163855d40da14a16ca80
NingGan <986097981@qq.com>
2021-11-21 00:54:34 +0800

    上传代码

--- src/permission.js
+++ src/permission.js
@@ -2,9 +2,8 @@ import router from "./router";
 import store from "./store";
 import NProgress from "nprogress";
 import "nprogress/nprogress.css";
-import {getToken, removeToken, setToken} from "@/utils/auth";
-import Cookies from 'js-cookie'
-import {tpTokenValidate, login, tokenValidate} from '@/api/login'
+import { getToken } from "@/utils/auth";
+import Vue from "vue";
 
 NProgress.configure({ showSpinner: false });
 
@@ -18,37 +17,8 @@ const whiteList = [
   "/bigScreen"
 ];
 
-// let userInfo = {
-//   "LOGIN_TOKEN": "",
-//   "USER_NAME": "",
-//   "USER_ID": "",
-//   "REAL_NAME": ""
-// }
-
 router.beforeEach((to, from, next) => {
-  if (to.path.includes('/logout/setToken')) {
-    // 删除 token
-    store.commit("SET_TOKEN", "");
-    store.commit("SET_REAL_NAME", "");
-    store.commit("SET_USERID", "");
-    store.commit("SET_ROLES", []);
-    store.commit("SET_PERMISSIONS", []);
-    removeToken();
-    return
-  }
-
-  if (to.path.includes('tpToken')) {
-    const { tpToken } = to.query
-    tpTokenValidate({tpToken, redirect_url: `${location.origin}/logout/setToken`}).then(res => {
-      const { code, obj } = res
-      if (code === 200) {
-        setWebToken(obj)
-      } else {
-        window.location = `https://yh.jxgsgl.com:6443/portal/v1/ssologin?redirect_url=${to.path}`
-      }
-    })
-  }
-
+  
   NProgress.start();
   if (getToken()) {
     /* has token*/
@@ -80,8 +50,7 @@ router.beforeEach((to, from, next) => {
               if (store.getters.userCheckedOrgIndex >= 0) {
                 next({ ...to, replace: true }); // hack方法 确保addRoutes已完成
               } else {
-                // next({ path: "/index" });
-                next({ path: "/bigScreen" })
+                next({ path: "/index" });
               }
             });
           })
@@ -103,22 +72,6 @@ router.beforeEach((to, from, next) => {
       }
     }
   } else {
-    const oldLoginTokens  = Cookies.get('loginTokens')
-    if (oldLoginTokens) {
-      tokenValidate({ loginToken: oldLoginTokens }).then(res => {
-        const { code, obj } = res
-        if (code === 200) {
-          // 生成 token ,存在前端本地
-          setWebToken(obj)
-        } else {
-          window.location = `https://yh.jxgsgl.com:6443/portal/v1/ssologin?redirect_url=${to.path}`
-        }
-      })
-    } else {
-      window.location = `https://yh.jxgsgl.com:6443/portal/v1/ssologin?redirect_url=${to.path}`
-    }
-
-
     // if (to.path === '/home') {
     //   next();
     //   return;
@@ -133,7 +86,7 @@ router.beforeEach((to, from, next) => {
       // 在免登录白名单，直接进入
       next();
     } else {
-      // next({ path: "/login", replace: true });
+      next({ path: "/login", replace: true });
       // next(`/login`); // 否则全部重定向到登录页
       // next(`/login?redirect=${to.fullPath}`); // 否则全部重定向到登录页
       NProgress.done();
@@ -154,23 +107,3 @@ router.onError(err => {
     console.log("error", err);
   }
 });
-
-/**
- * 设置本地 token
- * @param userInfo
- */
-function setWebToken (userInfo) {
-  store.commit('SET_REAL_NAME', obj.REAL_NAME)
-  store.commit('USER_NAME', obj.USER_NAME)
-  store.commit('SET_USERID', obj.USER_ID)
-  login({
-    loginName: userInfo.USER_NAME,
-    passWord: 'Zhzyq#2021',
-    redirect_uri: location.origin,
-    client_id: '08d69d53-6003-b0f7-c79a-523744000005'
-  }).then(res => {
-    // 生成 token ,存在前端本地
-    setToken(res.data.token);
-    store.commit("SET_TOKEN", res.data.token);
-  })
-}
